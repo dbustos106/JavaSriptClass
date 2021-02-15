84e32412e17ba7be90a997ffdf9be4d8ae315e85
Julian Bustos <dbustos@unal.edu.co>
2021-02-15 01:52:01 -0500

    Put

--- nodejs/enrutador.js
+++ nodejs/enrutador.js
@@ -17,6 +17,17 @@ module.exports = {
             global.recursos.mascotas.push(data.payload);
             callback(201, data.payload);
         },
+        put: (data, callback) => {  //handler
+            if (typeof data.indice != `undefined`) {
+                if (global.recursos.mascotas[data.indice]) {
+                    global.recursos.mascotas[data.indice] = data.payload;
+                    return callback(200, global.recursos.mascotas[data.indice]);
+                }
+                return callback(404, {mensaje: `No se encuentra la mascota ${data.indice}`});
+            }else{
+                callback(400, {mensaje: "Indice no enviado"});
+            }
+        },
     },
     noEncontrado: (data, callback) => {
         callback(404, { mensaje: `No encontrado` });
